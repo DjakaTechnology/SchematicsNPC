364a64733f8def62b9cfd35a648ddf2d650c4b3e
DTechnology <djakajaya89@gmail.com>
2018-08-13 21:13:14 +0700

    Updated for better answer

--- SumBinary.cpp
+++ SumBinary.cpp
@@ -4,16 +4,9 @@
 
 int main(){
 	long a, b;
-	std::string binA, binB, result ="";
 	scanf("%ld %ld", &a, &b);
-	binA = std::bitset<32>(a).to_string();
-	binB = std::bitset<32>(b).to_string();
 	
-	for(int i = 0;i < binA.length();i++){
-		result += binA.at(i) == binB.at(i) ? "0" : "1";
-	}
-	
-	printf("%ld\n", std::bitset<32>(result).to_ulong());
+	printf("%ld\n", a ^ b);
 
 	return 0;
 }
